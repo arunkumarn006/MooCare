5a863a353d1c694852bd3726b2b0ba7c518fcf60
Arun Kumar N <167411054+arunkumarn006@users.noreply.github.com>
2024-05-28 20:51:47 +0530

    appchange

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-
 from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
-sensor_data = {}
-
-@app.route('/api/sensor-data', methods=['POST'])
-def receive_sensor_data():
-    global sensor_data
-    sensor_data = request.json
-    return 'Sensor data received', 200
-
-@app.route('/api/sensor-data', methods=['GET'])
-def get_sensor_data():
-    return jsonify(sensor_data)
+sensor_data = {
+    "ir": 0,
+    "red": 0,
+    "ecg": 0,
+    "lat": None,
+    "lon": None
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/api/data', methods=['POST'])
+def receive_data():
+    global sensor_data
+    sensor_data = request.json
+    print('Received data:', sensor_data)
+    return '', 200
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(sensor_data)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
