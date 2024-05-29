056b992144c8213cc59c33b6b3f593e7818b8a63
Arun Kumar N <167411054+arunkumarn006@users.noreply.github.com>
2024-05-29 10:33:48 +0530

    newqpp

--- app.py
+++ app.py
@@ -1,61 +1,52 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross-Origin Resource Sharing
 
-# Setup SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-# Database model
+# Define the data model
 class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ir = db.Column(db.Integer, nullable=False)
-    red = db.Column(db.Integer, nullable=False)
+    ir = db.Column(db.Float, nullable=False)
+    red = db.Column(db.Float, nullable=False)
     ecg = db.Column(db.Integer, nullable=False)
-    lat = db.Column(db.Float)
-    lon = db.Column(db.Float)
+    lat = db.Column(db.Float, nullable=True)
+    lon = db.Column(db.Float, nullable=True)
 
-# Create the database
+# Create the database tables
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+# Define the route for POSTing data
 @app.route('/api/data', methods=['POST'])
-def receive_data():
-    data = request.json
-    sensor_data = SensorData(
-        ir=data['ir'],
-        red=data['red'],
-        ecg=data['ecg'],
-        lat=data.get('lat'),
-        lon=data.get('lon')
-    )
-    db.session.add(sensor_data)
+def post_data():
+    data = request.get_json()
+    ir = data['ir']
+    red = data['red']
+    ecg = data['ecg']
+    lat = data['lat']
+    lon = data['lon']
+    new_entry = SensorData(ir=ir, red=red, ecg=ecg, lat=lat, lon=lon)
+    db.session.add(new_entry)
     db.session.commit()
-    return '', 200
+    return jsonify({'message': 'Data received'}), 201
 
+# Define the route for GETting data
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    sensor_data = SensorData.query.order_by(SensorData.id.desc()).first()
-    if sensor_data:
-        return jsonify({
-            'ir': sensor_data.ir,
-            'red': sensor_data.red,
-            'ecg': sensor_data.ecg,
-            'lat': sensor_data.lat,
-            'lon': sensor_data.lon
-        })
-    else:
-        return jsonify({}), 404
+    results = SensorData.query.all()
+    data = [{
+        'id': result.id,
+        'ir': result.ir,
+        'red': result.red,
+        'ecg': result.ecg,
+        'lat': result.lat,
+        'lon': result.lon
+    } for result in results]
+    return jsonify(data), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
